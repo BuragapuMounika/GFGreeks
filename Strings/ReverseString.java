f4d4edbf0eef2ebac6a3badc67a27fa93e90008d
Buragapu Mounika <160312483+BuragapuMounika@users.noreply.github.com>
2025-05-14 15:09:44 +0530

    Add files via upload

---
+++ Strings/ReverseString.java
@@ -0,0 +1,13 @@
+public class ReverseString {
+
+    public static void main(String args[]){
+      String str ="Mounika";
+      StringBuilder sb = new StringBuilder(str);
+     String  reverse =sb.reverse().toString();
+      System.out.println(reverse);
+
+
+    }
+  
+    
+}
