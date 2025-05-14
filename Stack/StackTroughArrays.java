cd9ad6a96d42feae87100f730ec856bfa2711fc7
Buragapu Mounika <160312483+BuragapuMounika@users.noreply.github.com>
2025-05-14 15:13:25 +0530

    Add files via upload

---
+++ Stack/StackTroughArrays.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+  
+    class  Stack{
+        int arr[] = new int [5];
+        int top=-1;
+
+        public void push(int item){
+            if(top == arr.length-1){
+                
+            }
+        }
+
+    }
+}
+
---
+++ Stack/Stacks.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+public class Stacks {
+    public static void main(String args[]){
+        Stack <Integer> st= new Stack<>();
+
+        st.push(10);
+        st.push(100);
+        st.push(1000);
+        System.out.println(st);
+
+
+System.out.println(st.pop());
+System.out.println(st.isEmpty());
+
+
+    }
+    
+}
